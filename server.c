b19752728c24e6289d1df0bea3780d79b65ec2a2
uni-qzWang <48741909+uni-qzWang@users.noreply.github.com>
2019-05-07 16:14:17 +1000

    update http request handler

--- server.c
+++ server.c
@@ -47,7 +47,136 @@ typedef enum
     UNKNOWN
 } METHOD;
 
+/*
+	this function is adapted from the sample code on LMS, http-server.c,
+	and some changes are made.
+*/
+static bool handle_http_request(int sockfd)
+{
+	// try to read the request
+    char buff[2049];
+    int n = read(sockfd, buff, 2049);
+    if (n <= 0)
+    {
+        if (n < 0)
+            perror("read");
+        else
+            printf("socket %d close the connection\n", sockfd);
+        return false;
+    }
+	
+    // terminate the string
+    buff[n] = 0;
+	
+    char * req = buff;
+    
+    // parse the requestion method
+    METHOD method = UNKNOWN;
+    if (strncmp(req, "GET ", 4) == 0)
+    {
+        req += 4;
+        method = GET;
+    }
+    else if (strncmp(req, "POST ", 5) == 0)
+    {
+        req += 5;
+        method = POST;
+    }
+    else if (write(sockfd, HTTP_400, HTTP_400_LENGTH) < 0)
+    {
+        perror("write");
+        return false;
+    }
+    
+        // sanitise the URI
+    while (*req == '.' || *req == '/')
+        ++req;
+    if (*curr == ' ')
+        if (method == GET)
+        {
+            // get the size of the file
+            struct stat st;
+            stat("lab6-GET.html", &st);
+            n = sprintf(buff, HTTP_200_FORMAT, st.st_size);
+            // send the header first
+            if (write(sockfd, buff, n) < 0)
+            {
+                perror("write");
+                return false;
+            }
+            // send the file
+            int filefd = open("lab6-GET.html", O_RDONLY);
+            do
+            {
+                n = sendfile(sockfd, filefd, NULL, 2048);
+            }
+            while (n > 0);
+            if (n < 0)
+            {
+                perror("sendfile");
+                close(filefd);
+                return false;
+            }
+            close(filefd);
+        }
+        else if (method == POST)
+        {
+            // locate the username, it is safe to do so in this sample code, but usually the result is expected to be
+            // copied to another buffer using strcpy or strncpy to ensure that it will not be overwritten.
+            char * username = strstr(buff, "user=") + 5;
+            int username_length = strlen(username);
+            // the length needs to include the ", " before the username
+            long added_length = username_length + 2;
 
+            // get the size of the file
+            struct stat st;
+            stat("lab6-POST.html", &st);
+            // increase file size to accommodate the username
+            long size = st.st_size + added_length;
+            n = sprintf(buff, HTTP_200_FORMAT, size);
+            // send the header first
+            if (write(sockfd, buff, n) < 0)
+            {
+                perror("write");
+                return false;
+            }
+            // read the content of the HTML file
+            int filefd = open("lab6-POST.html", O_RDONLY);
+            n = read(filefd, buff, 2048);
+            if (n < 0)
+            {
+                perror("read");
+                close(filefd);
+                return false;
+            }
+            close(filefd);
+            // move the trailing part backward
+            int p1, p2;
+            for (p1 = size - 1, p2 = p1 - added_length; p1 >= size - 25; --p1, --p2)
+                buff[p1] = buff[p2];
+            ++p2;
+            // put the separator
+            buff[p2++] = ',';
+            buff[p2++] = ' ';
+            // copy the username
+            strncpy(buff + p2, username, username_length);
+            if (write(sockfd, buff, size) < 0)
+            {
+                perror("write");
+                return false;
+            }
+        }
+        else
+            // never used, just for completeness
+            fprintf(stderr, "no other methods supported");
+    // send 404
+    else if (write(sockfd, HTTP_404, HTTP_404_LENGTH) < 0)
+    {
+        perror("write");
+        return false;
+    }
+	
+}
 
 /* 
 	The main function here is adapted from the sample code, http-server.c,
